5fab96046d591ad16bdbc36a7898055ef5b293f1
lynn-thira <lynn.thira@gmail.com>
2017-06-13 15:47:21 +0300

    this contains the solution and test to fizzbuzz, all comments and suggestions are warmly welcome

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizz_buzz(number):
+    
+  if number%15 == 0: 
+    return "FizzBuzz"
+
+  elif number%3==0:
+    return "Fizz"
+
+  elif number%5==0:
+    return "Buzz"
+
+  else:
+    return number
